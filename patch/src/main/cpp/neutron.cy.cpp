d26fa32af013224462b2f4e38e5188da93a67a51
johnbanq <johnbanq@gmail.com>
2020-02-11 11:35:57 +0800

    Build: AS finally recognizes the cpp files!

--- patch/src/main/cpp/neutron.cy.cpp
+++ patch/src/main/cpp/neutron.cy.cpp
@@ -1,5 +1,15 @@
 #include "substrate.h"
+#include <string.h>
+#include <android/log.h>
 #include <jni.h>
+#include <stdio.h>
+#include <assert.h>
+
+#define  LOG_TAG    "native-dev"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
 //正在施工
 
 //algorithm
